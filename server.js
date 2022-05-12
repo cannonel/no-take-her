19a5bc4624c744c42aea402a64d19ed19a92ce05
Erika Cannon <cannone.rva@gmail.com>
2022-05-12 16:17:13 -0400

    server.js

--- server.js
+++ server.js
@@ -13,17 +13,17 @@ app.use(express.json());
 app.use(express.static('public'));
 // GET routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, '/public/assets/index.html'));
 });
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
+    res.sendFile(path.join(__dirname, '/public/assets/notes.html'));
 });
 app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/db/db.json'));
 });
 // wildcard route takes user back to root directory
 app.get('*', (req, res) => {
-    return res.sendFile(path.join(__dirname, 'public/index.html'));
+    return res.sendFile(path.join(__dirname, 'public/assets/index.html'));
 })
 // GET notes by ID from sidebar when clicked
 app.get('/api/notes/:id', (req, res) => {
